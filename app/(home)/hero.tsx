b943f44b0a7ce1fcbf84ea6d6fd5c7d15c974255
twostar-atdp <taylor.hickman@proton.me>
2024-07-01 14:13:30 +0000

    fixed unescaped apostrophe

--- app/(home)/hero.tsx
+++ app/(home)/hero.tsx
@@ -5,7 +5,7 @@ export function Hero() {
     <div className="flex flex-col items-center justify-start w-full min-h-[calc(100vh-80px)] md:min-h-[calc(100vh-130px)] p-7 pt-12">
       <div className="w-full max-w-2xl mx-auto text-center mb-8">
         <h2 className="text-xl text-gray-900 md:text-3xl mb-4">
-          Leave a note, we'll build the tracks
+          Leave a note, we&apos;ll build the tracks
         </h2>
         <Form />
       </div>
